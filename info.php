d2dbbbce208c71732676ed185e11bd9642c838f4
nara <nara@fmv-adin-PC-179>
2016-05-12 20:10:44 +0900

    navのデザイン・レイアウト変更前

---
+++ info.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="info.css" type="text/css"/>
+	<link rel="stylesheet" href="nav.css" type="text/css"/>
+	<link rel="stylesheet" href="sub_layout.css" type="text/css"/>
+	<link rel="shortcut icon" href="img/favicon.png">
+	<title>info</title>
+</head>
+<body>
+<div id="container">
+
+	<?php include "nav.html"; ?>
+	
+	<div id="contents">
+		<h1>Infomation</h1>
+		
+		<table border="1">
+			<caption>＊イタリア料理の店＊ Osteria Grazie ＊</caption>
+			<tr>
+				<th>住所</th>
+				<td>東京都代々木上原 xx-xx-xx</td>
+			</tr>
+			
+			<tr>
+				<th>電話</th>
+				<td>03-3468-xxxx</td>
+			</tr>
+			<tr>
+				<th>定休日</th>
+				<td>月曜日</td>
+			</tr>
+			<tr>
+				<th rowspan="2">営業時間</th>
+				<td>ランチ 11:00 ～ 2:00 まで</td>
+			</tr>
+			<tr>
+				<td>ディナー 5:00 ～ 11:00 まで</td>
+			</tr>
+			<tr>
+				<td colspan="2">当店は、小さな家をイメージした30席ほどのイタリア料理店です。お席のご予約は、お電話にて承っております。</td>
+			</tr>
+			<tr><td colspan="2">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.6736862984117!2d139.7270704504653!3d35.660410480102016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b770913970d%3A0xccc3467fcb15b353!2z44Kw44O844Kw44Or5qCq5byP5Lya56S-!5e0!3m2!1sja!2sjp!4v1460542761518" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</td></tr>
+		</table>
+		
+	</div>
+	
+</div>	
+	
+</body>
+</html>
